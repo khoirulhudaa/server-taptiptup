b75c8e868d6efc93e0ed8355e96d2de3237d9380
dukungin <dukung.in.support@gmail.com>
2026-05-08 14:25:11 +0700

    fix token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token) return res.status(403).json({ message: "Token diperlukan" });
 
-  jwt.verify(token, 'SECRET_KEY_KAMU', (err, decoded) => {
+  jwt.verify(token, 'DUKUNGINSERVER', (err, decoded) => {
     if (err) return res.status(401).json({ message: "Token tidak valid" });
     req.user = decoded; 
     next();
