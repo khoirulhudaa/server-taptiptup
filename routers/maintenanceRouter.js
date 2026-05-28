432748ab618896df931a6b08544a768afa16a278
dukungin <dukung.in.support@gmail.com>
2026-05-28 22:04:30 +0700

    fix

--- routers/maintenanceRouter.js
+++ routers/maintenanceRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { Maintenance } = require('../models');
 const router = express.Router();
-const Maintenance = require('../models');
-const auth = require('../middleware/authMiddleware');
+// const auth = require('../middleware/authMiddleware');
 
 // GET settings
-router.get('/settings', auth, async (req, res) => {
+router.get('/settings', async (req, res) => {
   try {
     let settings = await Maintenance.findOne().lean();
     if (!settings) {
@@ -17,7 +17,7 @@ router.get('/settings', auth, async (req, res) => {
 });
 
 // UPDATE settings (Hanya Super Admin)
-router.put('/settings', auth, async (req, res) => {
+router.put('/settings', async (req, res) => {
   try {
     const { auth, supporter, withdrawal, dashboard } = req.body;
 
@@ -36,6 +36,7 @@ router.put('/settings', auth, async (req, res) => {
 
     res.json({ success: true, data: settings });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
 });
