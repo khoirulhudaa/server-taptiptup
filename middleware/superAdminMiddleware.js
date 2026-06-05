1d1865f80d22e920155b29ce1df644040e019254
dukungin <dukung.in.support@gmail.com>
2026-06-05 08:57:48 +0700

    FIX: ROLE

--- middleware/superAdminMiddleware.js
+++ middleware/superAdminMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if (req.user?.role !== 'superAdmin') {
+  if (req.user?.role !== 'superAdmin' || req.user?.role !== 'streamerSuper') {
     return res.status(403).json({ message: 'Forbidden: SuperAdmin only' });
   }
   next();
