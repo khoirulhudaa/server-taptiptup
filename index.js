66559047d182e2641d27f3b82bdcf576e64c6679
dukungin <dukung.in.support@gmail.com>
2026-06-29 12:49:21 +0700

    fix

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ const streamerManageRoutes = require('./routers/streamerManagerRouter');
 const disbursementRouter = require('./routers/disbursementRouter');
 const dokuPaymentRouter = require('./routers/dokuPaymentRouter');
 const marqueeRoutes = require('./routers/marqueeRouter');
+const ipBlacklistRoutes = require('./routers/ipBlacklistRoute');
   
 app.get('/testing', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running!', node_env: process.env.NODE_ENV });
@@ -130,6 +131,7 @@ app.use('/api/wa', waRoutes);
 app.use('/api/suggestions', suggestionRoutes);
 app.use('/api/midtrans',     midtransRoutes);
 app.use('/api/marquee', marqueeRoutes);
+app.use('/api/ip-blacklist', ipBlacklistRoutes);
 app.use('/api/disbursement', disbursementRouter);
 app.use('/api/doku-payment', dokuPaymentRouter);
 app.use('/api/youtube-check',      youtubeCheckRoutes);
